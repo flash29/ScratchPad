99723ee92461f5f557d47204950e3c328340bfdc
Ranjeet Mallipeddi <43417434+flash29@users.noreply.github.com>
2020-10-13 20:03:55 +0530

    Updates

---
+++ precache-manifest.706fa2944eebe3f334430e8f8bd1e6df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7472b048af8649361869e27f61cb5772",
+    "url": "/ScratchPad/index.html"
+  },
+  {
+    "revision": "04ea175d0fd54d438845",
+    "url": "/ScratchPad/static/css/main.b54a69c1.chunk.css"
+  },
+  {
+    "revision": "69cc5646cd4bcb317780",
+    "url": "/ScratchPad/static/js/2.8428c97b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ScratchPad/static/js/2.8428c97b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04ea175d0fd54d438845",
+    "url": "/ScratchPad/static/js/main.b62a8ddf.chunk.js"
+  },
+  {
+    "revision": "96fa39409b2d29cf359d",
+    "url": "/ScratchPad/static/js/runtime-main.805e2c8e.js"
+  }
+]);
